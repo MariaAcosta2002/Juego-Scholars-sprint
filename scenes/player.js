ea0325d5fd6190e8347ebca0353de6cbb67ef2e3
MariaAcosta2002 <acostadelatorremariaguadalupe@gmail.com>
2024-07-02 07:09:43 -0600

    version 1.12

--- scenes/player.js
+++ scenes/player.js
@@ -4,24 +4,15 @@ export class Player{
     }
     
     preload(){
-        this.myScene.load.image('playerRunF1','../assets/img/Player/Captain/10-Run Sword/Run Sword 01.png')
-        this.myScene.load.image('playerRunF2','../assets/img/Player/Captain/10-Run Sword/Run Sword 02.png')
-        this.myScene.load.image('playerRunF3','../assets/img/Player/Captain/10-Run Sword/Run Sword 03.png')
-        this.myScene.load.image('playerRunF4','../assets/img/Player/Captain/10-Run Sword/Run Sword 04.png')
-        this.myScene.load.image('playerRunF5','../assets/img/Player/Captain/10-Run Sword/Run Sword 05.png')
-        this.myScene.load.image('playerRunF6','../assets/img/Player/Captain/10-Run Sword/Run Sword 06.png')
+        this.myScene.load.image('playerRunF1','../assets/img/Malo1/Malvado1.png')
+        this.myScene.load.image('playerRunF2','../assets/img/Malo1/Malvado3.png')
+        this.myScene.load.image('playerRunF3','../assets/img/Malo1/Malvado6.png')
     
-        this.myScene.load.image('playerIdleF1','../assets/img/Player/Captain/09-Idle Sword/Idle Sword 01.png')
-        this.myScene.load.image('playerIdleF2','../assets/img/Player/Captain/09-Idle Sword/Idle Sword 02.png')
-        this.myScene.load.image('playerIdleF3','../assets/img/Player/Captain/09-Idle Sword/Idle Sword 03.png')
-        this.myScene.load.image('playerIdleF4','../assets/img/Player/Captain/09-Idle Sword/Idle Sword 04.png')
-        this.myScene.load.image('playerIdleF5','../assets/img/Player/Captain/09-Idle Sword/Idle Sword 05.png')
+        this.myScene.load.image('playerIdleF1','../assets/img/MalO1/Malvado2.png')
         
-        this.myScene.load.image('playerJumpF1','../assets/img/Player/Captain/11-Jump Sword/Jump Sword 01.png')
-        this.myScene.load.image('playerJumpF2','../assets/img/Player/Captain/11-Jump Sword/Jump Sword 02.png')
-        this.myScene.load.image('playerJumpF3','../assets/img/Player/Captain/11-Jump Sword/Jump Sword 03.png')
-        
-        this.myScene.load.image('playerFallF1','../assets/img/Player/Captain/12-Fall Sword/Fall Sword 01.png')
+        this.myScene.load.image('playerJumpF1','../assets/img/Malo1/Malvado5.png')
+        this.myScene.load.image('playerJumpF2','../assets/img/Malo1/Malvado7.png')
+
     
     }
 
@@ -34,10 +25,7 @@ export class Player{
             frames: [
                 {key: 'playerRunF1'},
                 {key: 'playerRunF2'},
-                {key: 'playerRunF3'},
-                {key: 'playerRunF4'},
-                {key: 'playerRunF5'},
-                {key: 'playerRunF6'}
+                {key: 'playerRunF3'}
             ],
             frameRate: 10,
             repeat: -1
@@ -46,11 +34,7 @@ export class Player{
         this.myScene.anims.create({
             key:'Idle',
             frames: [
-                {key: 'playerIdleF1'},
-                {key: 'playerIdleF2'},
-                {key: 'playerIdleF3'},
-                {key: 'playerIdleF4'},
-                {key: 'playerIdleF5'}
+                {key: 'playerIdleF1'}
             ],
             frameRate: 10,
             repeat: -1
@@ -60,21 +44,12 @@ export class Player{
             key:'Jump',
             frames: [
                 {key: 'playerJumpF1'},
-                {key: 'playerJumpF2'},
-                {key: 'playerJumpF3'}
+                {key: 'playerJumpF2'}
             ],
             frameRate: 10,
             repeat: 1
         })
         //Fall
-        this.myScene.anims.create({
-            key:'Fall',
-            frames: [
-                {key: 'playerFallF1'}
-            ],
-            frameRate: 10,
-            repeat: 1
-        })
 
         //Physics-------------------------------------------------
         this.Player = this.myScene.physics.add.sprite(50,50, 'playerRunF1')
